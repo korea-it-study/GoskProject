cabbece8bb9b6092fecac591966341f0726572a9
qorals1121 <qorals1121@naver.com>
2022-12-21 15:13:12 +0900

    제발요...

---
+++ src/main/java/com/Gosk/GoskProject20221221/GoskProject20221221Application.java
@@ -0,0 +1,13 @@
+package com.Gosk.GoskProject20221221;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoskProject20221221Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GoskProject20221221Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/Gosk/GoskProject20221221/GoskProject20221221ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Gosk.GoskProject20221221;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GoskProject20221221ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
